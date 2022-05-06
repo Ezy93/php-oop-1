eb0943af1aa3336c4189fe9686c643a17d01ddb7
Ezy93 <giordani001@gmail.com>
2022-05-06 14:58:29 +0200

    aggiunti metodi alla classe creata chiamata Movie

--- index.php
+++ index.php
@@ -21,6 +21,30 @@ class Movie
             $this->director = $director;
             $this->nationality = $nationality;
         }
+
+        public function getTitle(){
+            return $this->title;
+        }
+
+        public function getLanguage(){
+            return $this->language;
+        }
+
+        public function getDuration(){
+            return $this->duration;
+        }
+
+        public function getActors(){
+            return $this->actors;
+        }
+
+        public function getDirector(){
+            return $this->director;
+        }
+
+        public function getNationality(){
+            return $this->nationality;
+        }
         
     }
     
