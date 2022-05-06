7b0266c005b8f423bfeda743b3c23d9981f8bcc4
Ezy93 <giordani001@gmail.com>
2022-05-06 14:52:15 +0200

    aggiunto constructor alla class Movie

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?php  
-    class Movie{
+
+
+class Movie
+    {
         private $title;
         private $language;
         private $duration;
         private $actors;
         private $director;
         private $nationality;
+        
+        
+        
+        public function __construct($title, $language, $duration, $actors, $director, $nationality)
+        {
+            $this->title = $title;
+            $this->language = $language;
+            $this->duration = $duration;
+            $this->actors = $actors;
+            $this->director = $director;
+            $this->nationality = $nationality;
+        }
+        
     }
+    
 ?>
